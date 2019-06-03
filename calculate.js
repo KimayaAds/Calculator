b798b8ca99f25575e4617887175868689cffa650
KimayaAds <50841216+KimayaAds@users.noreply.github.com>
2019-06-03 16:20:06 -0400

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,115 +1,115 @@
 var firstOperand = "";
 var secondOperand = "";
-var result="";
+var result = "";
 var operator = "";
-var queuedoperator = 0;
+var queuedOperator = 0;
 var calFinished = false;
- 
-//Function to access input
-function input(num) {
 
-    var display = document.getElementById("display");
-  
-    
+//Function to access   inputNumber
+function inputNumber(num) {
 
-   // checks if the operation is finished or not .ie. for queued operation
-        if (calFinished == true) {
+    display === document.getElementById("display");
+
+    firstOperand = parseFloat(firstOperand);
+    secondOperand = parseFloat(secondOperand);
+
+
+    // checks if the operation is finished or not .ie. for queued operation
+    if (calFinished == true) {
         display.value = num; // queuedOperation
         calFinished = false;
-        }
+    }
 
-        // displays the key pressed on the display screen
-            else {
+    // displays the key pressed on the display screen
+    else {
         display.value += num; // 
-         }
-             
-        } 
-        
+    }
+
+}
+
 function setOperator(command) {
 
     // Screen display 
 
     var display = document.getElementById("display"),
- 
-    firstOperand =  display.value;
-     
-        
-    if (command == 'add') {
-        operator = 1;
-    
-    }
-        else if (command == 'subtract') {
-        operator = 2;
-        }
-    if (command == 'multiply') {
-        operator = 3;
-    } 
-    if (command == 'divide') {
-        operator = 4;
+
+        firstOperand = display.value;
+
+    switch (command) {
+        case 'add': operator = 1;
+            break;
+
+        case 'subtract': operator = 2;
+            break;
+
+        case 'multiply': operator = 3;
+            break;
+
+        case 'divide': operator = 4;
+            break;
     }
 
-// firstOperand is stored in new variable evalSecondOperand
-    var evalSecondOperand = eval(firstOperand),
-        evalFirstOperand = eval(secondOperand);
 
-    if (queuedoperator == 0) {
+
+
+    // firstOperand is stored in new variable newSecondOperand
+    var newSecondOperand = eval(firstOperand);
+    var newFirstOperand = eval(secondOperand);
+    if (queuedOperator == 0) {
         secondOperand = display.value;
     }
-    if (queuedoperator == 1) {
-        secondOperand = evalFirstOperand + evalSecondOperand;
+
+
+    switch (queuedOperator) {
+
+
+        case 1: secondOperand = newFirstOperand + newSecondOperand;
+            break;
+
+        case 2: secondOperand = newFirstOperand - newSecondOperand;
+            break;
+
+        case 3: secondOperand = newFirstOperand * newSecondOperand;
+            break;
+
+        case 4: secondOperand = newFirstOperand / newSecondOperand;
+            break;
+
     }
-        else if (queuedoperator == 2) {
-            secondOperand = evalFirstOperand - evalSecondOperand;
-            }
-            else if (queuedoperator == 3) {
-                secondOperand = evalFirstOperand * evalSecondOperand;
-            }
-
-                else if (queuedoperator == 4) {
-     
-                  secondOperand = evalFirstOperand / evalSecondOperand;
-        
-                }
-
 
     // Then set the operator to the correct number
-                display.value =+ operator;
-        queuedoperator = operator;
+    display.value = + operator;
+    queuedOperator = operator;
 
-        // clear the display in order to recieve the next input 
-        display.value = '';
+    // clear the display in order to recieve the next   inputNumber  
+    display.value = '';
 }
 
 // Calculate function ()
 
 function calculate() {
 
-    var display = document.getElementById("display");
+    display === document.getElementById("display");
     firstOperand = display.value;
-    var evalSecondOperand = eval(firstOperand),
-    evalFirstOperand = eval(secondOperand);
+    var newSecondOperand = eval(firstOperand),
+        newFirstOperand = eval(secondOperand);
 
-    if (operator == " ") {
-            alert("Select numbers for calcualtion");
-            return;
+    switch (operator) {
+
+
+        case 1: firstOperand = newFirstOperand + newSecondOperand;
+            break;
+
+        case 2: firstOperand = newFirstOperand - newSecondOperand;
+            break;
+
+        case 3: firstOperand = newFirstOperand * newSecondOperand;
+            break;
+
+        case 4: firstOperand = newFirstOperand / newSecondOperand;
+            break;
+
     }
-
-    if (operator == 1) {
-        
-            firstOperand = evalFirstOperand + evalSecondOperand;  
-    
-    }
-
-         else if (operator == 2) {
-
-        firstOperand = evalFirstOperand - evalSecondOperand;
-        }
-            else if (operator == 3) {
-            firstOperand = evalFirstOperand * evalSecondOperand;
-            }
-                else if (operator == 4) {
-                firstOperand = evalFirstOperand / evalSecondOperand;
-                 }
 
     display.value = firstOperand;
     result = firstOperand;
@@ -118,32 +118,24 @@ function calculate() {
         calFinished = true;
 
     operator = 0;
-    queuedoperator = 0;
+    queuedOperator = 0;
     firstOperand = "";
     secondOperand = "";
 }
 
+///Function for Decimals
 
-///Function for Deci    mals
+function decimal(dec) {
+    display === document.getElementById("display");
 
-function decimal(dec    ) {
-    var display = document.getElementById("display");
-
-    // Loop through the current number to make sure there isn't already a decimal
-    for (i = 0; i < display.value.length; i++)
-        if (display.value.charAt(i) == '.') {
-        
-            return;
-    }
-
-        display.value += dec;
+    display.value += dec;
 }
 
 // Store the result 
-function previousResult(){
+function previousResult() {
     var display = document.getElementById("display");
 
-    display.value=result;
+    display.value = result;
 }
 
 //Function to reset
